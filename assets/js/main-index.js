bf284055e309e9c488f77cf412fda9d98a87a384
liulipack <liulipack@qq.com>
2021-03-20 13:37:35 +0800

    移除多余的“小屏设备横屏提示”功能。

--- assets/js/main-index.js
+++ assets/js/main-index.js
@@ -13,12 +13,6 @@ setTimeout(function() {
     LANCheck.parentNode.removeChild(LANCheck);
 },1000);
 
-// 横屏提示
-let tips_btn = document.querySelector('#tips .close');
-tips_btn.addEventListener('click', function() {
-    tips_btn.parentNode.remove()
-});
-
 // 打字效果
 ityped.init('#index #iTyped', {
     strings:['里·动画', '表·动画'/*, '漫画(筹备中)', '游戏(筹备中)'*/, '鹰小队'],
