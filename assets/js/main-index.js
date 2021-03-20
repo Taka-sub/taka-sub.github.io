e849825b4e95c98ef8550bf7cd550ce331152d63
liulipack <liulipack@qq.com>
2021-03-20 13:36:24 +0800

    小优化。
    添加小屏设备横屏提示。(没啥用，待会儿就移除)
    优化 HTML 的元数据(meta)。
    索引页阻止选中标题，但同时鼠标悬停时还是会改变样式。

--- assets/js/main-index.js
+++ assets/js/main-index.js
@@ -8,10 +8,16 @@ function isWAN() {
 document.querySelector('body').innerHTML += '<img src="https://i.imgur.com/xL9Y87T.gif" onload="isWAN()" id="LANCheck" style="display: none;">'
 
 setTimeout(function() {
-    let LANCheck = document.getElementById('LANCheck')
+    let LANCheck = document.getElementById('LANCheck');
     LANCheck.removeAttribute('src');
     LANCheck.parentNode.removeChild(LANCheck);
 },1000);
+
+// 横屏提示
+let tips_btn = document.querySelector('#tips .close');
+tips_btn.addEventListener('click', function() {
+    tips_btn.parentNode.remove()
+});
 
 // 打字效果
 ityped.init('#index #iTyped', {
