36b105eb36dd3f8d890168be324ae7efb948a6ab
liulipack <liulipack@qq.com>
2021-04-07 20:32:17 +0800

    更新，仅此而已。 Updates, that's all.

--- assets/js/main-index.js
+++ assets/js/main-index.js
@@ -1,18 +1,3 @@
-// 外网访问检测
-function isWAN() {
-    document.querySelectorAll('a[hidden]').forEach((elm) => {
-        elm.removeAttribute('hidden')
-    });
-};
-
-document.querySelector('body').innerHTML += '<img src="https://i.imgur.com/xL9Y87T.gif" onload="isWAN()" id="LANCheck" style="display: none;">'
-
-setTimeout(function() {
-    let LANCheck = document.getElementById('LANCheck');
-    LANCheck.removeAttribute('src');
-    LANCheck.parentNode.removeChild(LANCheck);
-},1000);
-
 // 打字效果
 ityped.init('#index #iTyped', {
     strings:['里·动画', '表·动画'/*, '漫画(筹备中)', '游戏(筹备中)'*/, '鹰小队'],
--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,42 @@ document.querySelector('body').addEventListener('DOMNodeInserted', function() {
         }
     });
 });
+
+// 写入抽屉栏导航
+$('#drawer').html('<ul class="mdui-list">\
+        <li class="mdui-subheader">年份</li>\
+        <a href="2021.html"><li class="mdui-list-item mdui-ripple mdui-list-item-active">2021年</li></a>\
+        <a href="2020.html"><li class="mdui-list-item mdui-ripple">2020年</li></a>\
+    </ul>\
+    <ul class="mdui-list">\
+        <li class="mdui-subheader">传送门</li>\
+        <a hidden href="https://t.me/joinchat/VIDkLgNgoSsWYM4Z"><li class="mdui-list-item mdui-ripple">Telegram 群组</li></a>\
+        <a href="https://jq.qq.com/?_wv=1027&k=lon6Dfsc"><li class="mdui-list-item mdui-ripple">QQ 群组</li></a>\
+        <a hidden href="https://nyaa.si/user/ointment"><li class="mdui-list-item mdui-ripple">Nyaa·表区</li></a>\
+        <a hidden href="https://sukebei.nyaa.si/user/ointment"><li class="mdui-list-item mdui-ripple">Nyaa·里区</li></a>\
+        <a href="https://space.bilibili.com/343162729/dynamic"><li class="mdui-list-item mdui-ripple">哔哩哔哩</li></a>\
+        <a href="https://m.weibo.cn/u/7369491931"><li class="mdui-list-item mdui-ripple">微博</li></a>\
+    </ul>\
+    <ul class="mdui-list">\
+        <li class="mdui-subheader">其他</li>\
+        <a href="Tools.html"><li class="mdui-list-item mdui-ripple">工具集</li></a>\
+    </ul>\
+    <div class="copyright">\
+        <p>&copy; 2021 Taka.sub</p>\
+        <p>Powered by <a href="https://mdui.org" target="_blank">MDUI</a> &amp; <a href="https://www.muicss.com" target="_blank">MUI</a> &amp; <a href="https://github.com/luisvinicius167/ityped" target="_blank">iTyped</a></p>\
+    </div>')
+
+// 外网访问检测
+function isWAN() {
+    document.querySelectorAll('a[hidden]').forEach((elm) => {
+        elm.removeAttribute('hidden')
+    });
+};
+
+document.querySelector('body').innerHTML += '<img src="https://i.imgur.com/xL9Y87T.gif" onload="isWAN()" id="LANCheck" style="display: none;">'
+
+setTimeout(function() {
+    let LANCheck = document.getElementById('LANCheck');
+    LANCheck.removeAttribute('src');
+    LANCheck.parentNode.removeChild(LANCheck);
+},1000);
