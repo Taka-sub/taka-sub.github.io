4928b2e46e9814826f67cb058e3a7da5ddda167e
liulipack <liulipack@qq.com>
2021-06-12 15:39:43 +0800

    移除旧内容。

--- old/assets/js/main-index.js
+++
@@ -1,8 +0,0 @@
-// 打字效果
-ityped.init('#index #iTyped', {
-    strings:['里·动画', '表·动画'/*, '漫画(筹备中)', '游戏(筹备中)'*/, '鹰小队'],
-    startDelay: 500,
-    backDelay:  2500,
-    backSpeed:  100,
-    loop: true
-});
