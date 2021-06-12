f17af8c53bba12953a684d3d6b3025bf41d818e8
liulipack <liulipack@qq.com>
2021-06-12 18:30:06 +0800

    优化彩蛋页随机视频算法，并支持手动修改。修改 DPlayer 代码，使得片段切换描述更符合使用场景。

--- assets/js/main-easter.js
+++ assets/js/main-easter.js
@@ -3,18 +3,38 @@ const dp = new DPlayer({
     container: document.getElementById('dplayer'),
     screenshot: true,
     video: {
-        url: 'assets/videos/Naroto-Clips '+eval(Math.floor(Math.random() * 5) + 1)+'.mp4',
-        autoplay: true,
-        loop: true,
-        volume: 0.2
-    },
-    contextmenu: [
-        {
-            text: '再来一段',
-            click: () => {
-                location.reload();
+        quality: [
+            {
+                name: '片段 1',
+                url: 'assets/videos/Naroto-Clips 1.mp4',
+                type: 'normal'
             },
-        },
+            {
+                name: '片段 2',
+                url: 'assets/videos/Naroto-Clips 2.mp4',
+                type: 'normal'
+            },
+            {
+                name: '片段 3',
+                url: 'assets/videos/Naroto-Clips 3.mp4',
+                type: 'normal'
+            },
+            {
+                name: '片段 4',
+                url: 'assets/videos/Naroto-Clips 4.mp4',
+                type: 'normal'
+            },
+            {
+                name: '片段 5',
+                url: 'assets/videos/Naroto-Clips 5.mp4',
+                type: 'normal'
+            }
+        ],
+        defaultQuality: Math.round(Math.random() * 4)
+    },
+    preload: 'metadata',
+    volume: 0.2,
+    contextmenu: [
         {
             text: '返回导航页',
             link: 'nav.html',
