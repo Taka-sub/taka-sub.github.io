8d1d4c7baa64c822a1d11dd40dd79a543f402ca4
liulipack <liulipack@qq.com>
2021-07-06 21:03:33 +0800

    更新首页索引页内容。

--- assets/js/main-index.js
+++ assets/js/main-index.js
@@ -1,6 +1,6 @@
 // 打字效果
 ityped.init('#index #iTyped', {
-    strings:['里·动画', '表·动画', '漫画'/*, '游戏(筹备中)'*/, '鹰小队'],
+    strings:['动画', '漫画', '游戏', '鹰小队'],
     startDelay: 500,
     backDelay:  1500,
     backSpeed:  100,
